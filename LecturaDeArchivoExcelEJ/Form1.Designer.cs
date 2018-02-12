96a40ac813ea0047eaeb7671ce303acfdbcdcdf4
Adrian Garcia <adrian_gar016@hotmail.es>
2018-02-11 18:49:40 -0600

    Initial commit

---
+++ LecturaDeArchivoExcelEJ/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace LecturaDeArchivoExcelEJ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.botonBuscar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.botonLeerExcel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // botonBuscar
+            // 
+            this.botonBuscar.Location = new System.Drawing.Point(23, 27);
+            this.botonBuscar.Name = "botonBuscar";
+            this.botonBuscar.Size = new System.Drawing.Size(75, 23);
+            this.botonBuscar.TabIndex = 0;
+            this.botonBuscar.Text = "Buscar";
+            this.botonBuscar.UseVisualStyleBackColor = true;
+            this.botonBuscar.Click += new System.EventHandler(this.botonBuscar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(134, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(481, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // botonLeerExcel
+            // 
+            this.botonLeerExcel.Location = new System.Drawing.Point(622, 26);
+            this.botonLeerExcel.Name = "botonLeerExcel";
+            this.botonLeerExcel.Size = new System.Drawing.Size(105, 23);
+            this.botonLeerExcel.TabIndex = 2;
+            this.botonLeerExcel.Text = "Leer Excel";
+            this.botonLeerExcel.UseVisualStyleBackColor = true;
+            this.botonLeerExcel.Click += new System.EventHandler(this.botonLeerExcel_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ruta";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(18, 82);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(803, 336);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(849, 446);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.botonLeerExcel);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.botonBuscar);
+            this.Name = "Form1";
+            this.Text = "Leer archivo de excel - ejemplo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonBuscar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button botonLeerExcel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ LecturaDeArchivoExcelEJ/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+//No olvides agregar al proyecto la referencia Microsoft.Office.Interop.Excel e incluirla en el proyecto
+
+namespace LecturaDeArchivoExcelEJ
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void botonBuscar_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = @"C:\";
+            openFileDialog1.Title = "Buscar";
+
+            openFileDialog1.CheckFileExists = true;
+            openFileDialog1.CheckPathExists = true;
+
+            openFileDialog1.DefaultExt = "xls";
+            openFileDialog1.Filter = "Archivos de Excel (*.xls)|*.xls|Todos (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            openFileDialog1.ReadOnlyChecked = true;
+            openFileDialog1.ShowReadOnly = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void botonLeerExcel_Click(object sender, EventArgs e)
+        {
+            String rutaDelArchivo = textBox1.Text;
+            if (rutaDelArchivo.Length > 0)
+            {
+                recorrerArchivoExcel(rutaDelArchivo);
+            }
+            else
+            {
+                MessageBox.Show("Primero debes seleccionar un archivo de Excel");
+            }
+        }
+
+        public void recorrerArchivoExcel(String rutaDelArchivo)
+        {
+            Excel.Application excelApplicacion;
+            Excel.Workbook excelWorkBook;
+            Excel.Worksheet excelHoja;
+            Excel.Range excelRango;
+
+
+            Int32 rowCnt = 0;
+            Int32 colCnt = 0;
+
+            try
+            {
+                excelApplicacion = new Excel.Application();
+                excelWorkBook = excelApplicacion.Workbooks.Open(rutaDelArchivo, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+                excelHoja = (Excel.Worksheet)excelWorkBook.Worksheets.get_Item(1);
+
+                excelRango = excelHoja.UsedRange;
+                Int32 rangoEnColumnas = excelRango.Columns.Count;
+                Int32 rangoEnFilas = excelRango.Rows.Count;
+                
+
+                if (rangoEnFilas > 0 && rangoEnColumnas > 0)
+                {
+
+                    String valorDeCelda = "";
+                    
+
+                    
+                }
+                else
+                {
+                    MessageBox.Show("El archivo no contiene información");
+                }
+
+                excelWorkBook.Close(true, null, null);
+                excelApplicacion.Quit();
+
+                releaseObject(excelHoja);
+                releaseObject(excelWorkBook);
+                releaseObject(excelApplicacion);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Debes contar con Microsoft Office en tu equipo!" + Environment.NewLine + e);                
+            }
+
+        }
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception e)
+            {
+                obj = null;
+                MessageBox.Show("No es posible liberar objeto" + e);
+            }
+            finally
+            {
+                GC.Collect();
+            }
+        }
+    }
+}
